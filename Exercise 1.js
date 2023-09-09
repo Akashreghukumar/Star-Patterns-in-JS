53154a7726d8e1fc4aaad1a3894bfdd0464df426
Akashreghukumar <47936922+Akashreghukumar@users.noreply.github.com>
2023-09-09 20:45:55 +0530

    Create Exercise 1.js

---
+++ Exercise 1.js
@@ -0,0 +1,22 @@
+*****
+*****
+*****
+*****
+*****
+
+CODE
+
+let n=5
+let str=""
+
+for(let i=1;i<=n;i++)
+{
+  for(let j=1;j<=n;j++)
+  {
+   str=str+"*" 
+  }
+  str= str + "\n"
+}
+
+console.log(str)
+
